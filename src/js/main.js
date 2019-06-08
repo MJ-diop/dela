6388bf4388ac5933208259114dfabd7b93bcc8ef
M_J_D <diopmbayejacques9@gmail.com>
2019-06-08 17:34:56 +0000

    PRESATATION: modal is done

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,20 @@
-$(function () {
-    $('.navbar a, footer a').on('click', function (event) {
-        event.preventDefault();
-        let hash = this.hash;
-        $('body,html').animate({
-            scrollTop: $(hash).offset().top
-        }, 900, function () {
-            window.location.hash = hash;
-        })
-    });
-})
+$(function() {
+  $('.navbar a, footer a').on('click', function(e) {
+    e.preventDefault();
+    let hash = this.hash;
+    $('body,html').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      900,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  });
+
+  // MODAAL
+  $('#exampleModalScrollable').on('shown.bs.modal', function() {
+    $('#myInput').trigger('focus');
+  });
+});
